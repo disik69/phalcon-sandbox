5035a18fc3f125f23c5bd7c2caab3439e60792fa
disik <disik69@rambler.ru>
2015-08-03 13:47:19 +0300

    base structure

--- bootstrap/application.php
+++ bootstrap/application.php
@@ -1,40 +1,39 @@
 <?php
 
 use \Phalcon\Loader;
+use \Phalcon\Debug;
 use \Phalcon\Mvc\Application;
 use \Phalcon\Config\Adapter\Ini as ConfigIni;
 
-try {
-    include_once BASE_PATH . 'vendor/autoload.php';
-    
-    /**
-     * Read the configuration
-     */
-    $config = new ConfigIni(BASE_PATH . 'app/config/config.ini');
-    
-    // Register an autoloader
-    $loader = new Loader();
-    
-    $loader->registerDirs(
-        array(
-            BASE_PATH . 'app/controllers/',
-            BASE_PATH . 'app/models/',
-            BASE_PATH . 'app/plugins/',
-            BASE_PATH . 'app/extends/',
-        )
-    )->register();
+include_once BASE_PATH . 'vendor/autoload.php';
 
-    /**
-     * Load application services
-     */
-    $di = require(BASE_PATH . 'bootstrap/di.php');
-    
-    $application = new Application($di);
-    
-    echo $application->handle()->getContent();
-} catch (Exception $e) {
-    if ($config->app->debug) {
-        echo get_class($e) . ': ' . $e->getMessage();
-    }
+/**
+ * Read the configuration
+ */
+$config = new ConfigIni(BASE_PATH . 'app/config/config.ini');
+
+if ($config->app->debug) {
+    $debug = new Debug(); 
+    $debug->listen();
 }
 
+// Register an autoloader
+$loader = new Loader();
+
+$loader->registerDirs(
+    array(
+        BASE_PATH . 'app/controllers/',
+        BASE_PATH . 'app/models/',
+        BASE_PATH . 'app/plugins/',
+        BASE_PATH . 'app/extends/',
+    )
+)->register();
+
+/**
+ * Load application services
+ */
+$di = require(BASE_PATH . 'bootstrap/di.php');
+
+$application = new Application($di);
+
+echo $application->handle()->getContent();
