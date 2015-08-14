45d9761f25fc3c1b7db23dddbf53368274bbfd83
disik <disik69@rambler.ru>
2015-08-14 18:08:42 +0300

    base structure

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -5,6 +5,8 @@ class ErrorController extends \ControllerBase
     public function notFoundAction()
     {
         $this->view->pick('error/not-found');
+        
+        $this->response->setStatusCode(404);
     }
     
     public function unauthorizedAction()
